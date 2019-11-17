c007501cc69a8945f36387743cc8ca05f03f389d
RKNyra <rk.nyra@gmail.com>
2019-11-17 20:02:54 +0300

    install a database dependency - psycopg2, create the app's database, change the database settings to use postgres and run the first migration ... all these so as to connect Postgres to the application

--- instaklooni/settings.py
+++ instaklooni/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'instaklooni.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'instaklooni',
         'USER': 'rknyra',
         'PASSWORD': 'rknyra7',
