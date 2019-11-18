b2173c332cf3077d7b2f771f02460afbfa813a50
RKNyra <rk.nyra@gmail.com>
2019-11-18 04:09:01 +0300

    create the image model with the necessary properties to enable users to upload, like and commenton images posted

--- klooni/models.py
+++ klooni/models.py
@@ -8,3 +8,13 @@ class Profile(models.Model):
     prof_pic = ImageField(blank=True, manual_crop="")
     bio = models.CharField(max_length = 200)
     
+
+#Image Model
+class Image(models.Model):
+    image = ImageField(blank=True, manual_crop="")
+    name = models.CharField(max_length =70)
+    caption = models.CharField(max_length =700)
+    post_date = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    likes = models.ManytToMany(User, related_name='likes', blank=True)
+    comments = models.ManytToMany(User, related_name='comments', blank=True)
