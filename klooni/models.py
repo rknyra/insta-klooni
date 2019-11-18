67c32f9625ac7067075cb395691374572f34888e
RKNyra <rk.nyra@gmail.com>
2019-11-18 04:07:36 +0300

    create the profile model which will enable users to update their prof-pic and bio properties

--- klooni/models.py
+++ klooni/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
+from pyuploadcare.dj.models import ImageField
 
-# Create your models here.
+#Profile model
+class Profile(models.Model):
+    prof_pic = ImageField(blank=True, manual_crop="")
+    bio = models.CharField(max_length = 200)
+    
