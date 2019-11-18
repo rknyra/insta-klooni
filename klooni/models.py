0f5d0a120d20d33de470395dd3e94af9ba72cd67
RKNyra <rk.nyra@gmail.com>
2019-11-18 07:00:37 +0300

    write the tests to confirm that an image is can be deleted after being uploaded by a user and define the delete_image method that enables the deletion of the image

--- klooni/models.py
+++ klooni/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import datetime as dt
 from pyuploadcare.dj.models import ImageField
+from django.shortcuts import get_object_or_404
 
 #Profile model
 class Profile(models.Model):
@@ -20,7 +21,14 @@ class Image(models.Model):
     comments = models.CharField(max_length =200, blank=True)
     
     def save_image(self):
-            self.save()
+        self.save()
+            
+    # def update_image(self):
+    #     self._do_update()
+    
+    def delete_image(self):
+        # img = get_object_or_404(Image,pk=Image.id)
+        img=Image.objects.filter(id=Image.id).delete()
    
     class Meta:
         ordering = ['post_date']
--- klooni/tests.py
+++ klooni/tests.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
 from .models import Profile, Image
+from django.shortcuts import get_object_or_404
+
 
 class ImageTestClass(TestCase):
     #set-up method
     def setUp(self):
-        self.img = Image(name='imgOne',caption='capOne', likes=3, comments='clever!', profile_id=1)
+        self.img = Image(id=1, name='imgOne',caption='capOne', likes=3, comments='clever!', profile_id=1)
         self.img.save_image()
+        self.img.delete()
         
     #testing  instance
     def test_instance(self):
@@ -16,6 +19,22 @@ class ImageTestClass(TestCase):
         self.img.save_image()
         img = Image.objects.all()
         self.assertTrue(len(img) > 0)
+        
+    #testing the update_method
+    # def test_update_method(self):
+    #     self.img.update_image()
+
+  #set-up method
+    def setUp(self):
+        self.img = Image(id=1, name='imgOne',caption='capOne', likes=3, comments='clever!', profile_id=1)
+        self.img.save()
+        self.img.delete()
+        
+    #testing the delete_method
+    def test_delete_method(self):
+        # img = get_object_or_404(Image,pk=1)
+        img=Image.objects.filter(id=1).delete()
+        self.assertTrue(len(img) == 1)
     
     #tear-down the instance
     def tearDown(self):
